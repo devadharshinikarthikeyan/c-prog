7c9a3dececf99d461b7d1350d43b0e963ad47bba
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-21 05:54:38 +0000

    Create armstrong no 2.c

---
+++ armstrong no 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int num,rem,result=0;
+scanf("%d",&rem);
+while(num!=0)
+{
+rem=num%10;
+result=result+rem*rem*rem;
+num=num/10;
+}
+if(result==num)
+printf("armstrong no");
+else
+printf("not armstrong no");
+}
