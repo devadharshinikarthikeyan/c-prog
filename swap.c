2990b857edd247499bdc67aeea89100663c44405
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-28 15:26:45 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() 
+{
+   int a, b, temp;
+   printf("Enter the value of a and b: ");
+   scanf("%d %d", &a, &b);
+   printf("Before swapping a=%d, b=%d ", x, y);
+    
+temp = x;
+   x = y;
+   y = temp;
+   printf("After swapping x=%d, b=%d", x, y);
+   return 0; 
+}
