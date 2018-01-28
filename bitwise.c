b908a5ce80a8e3c8dcb82edf5f4cb4c11f42c71e
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-28 15:29:40 +0530

    Create bitwise.c

---
+++ bitwise.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a = 65;
+    int b = 120;
+    printf(" value of i=%d k=%d before swapping", i, k);
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    printf("value of a=%d b=%d after swapping", a, b-);
+
+    return 0;
+    }
