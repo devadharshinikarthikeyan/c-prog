15d010c276a4914c54374593aa6e006f8591bed9
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-01-28 15:30:08 +0530

    Update bitwise.c

--- bitwise.c
+++ bitwise.c
@@ -2,12 +2,12 @@
 int main() {
     int a = 65;
     int b = 120;
-    printf(" value of i=%d k=%d before swapping", i, k);
+    printf(" value of i=%d k=%d before swapping", a, b);
 
     a = a ^ b;
     b = a ^ b;
     a = a ^ b;
-    printf("value of a=%d b=%d after swapping", a, b-);
+    printf("value of a=%d b=%d after swapping", a, b);
 
     return 0;
     }
