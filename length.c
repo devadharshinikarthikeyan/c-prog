c3f8d7439199c5aad23ac82c72ff3f351c30ddfc
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-05 15:34:16 +0530

    Create length.c

---
+++ length.c
@@ -0,0 +1,16 @@
+void main()
+{
+char str[100];
+int len, space;
+
+printf( "Enter string:\n ");
+printf("\n");
+gets(str);
+
+len = strlen(str);
+
+printf("\n");
+printf("\n No.of characters in string is %d " , len );
+
+getch();
+}
