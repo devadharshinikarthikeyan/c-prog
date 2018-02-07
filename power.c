d6222613f875b1128584cd09277936c26e57772a
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-07 16:04:18 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,13 +1,24 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+
+int main()
 {
-int base,exponent,result;
-prinf("enter the number");
-scanf("%d%d",&base,&exponent);
-while(exponent!=0)
-{
-result=base;
---exponent;
-}
-printf("%d",result):
+    int base, exponent;
+    long  power = 1;
+    int i;
+
+   
+    printf("Enter base: ");
+    scanf("%d", &base);
+    printf("Enter exponent: ");
+    scanf("%d", &exponent);
+
+    
+    for(i=1; i<=exponent; i++)
+    {
+        power = power * base;
+    }
+
+    printf("%d ^ %d = %lld", base, exponent, power);
+
+    return 0;
 }
