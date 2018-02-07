a961825ad5f1e8bf71dc4f0d2bd73004a2cd0a85
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-07 15:37:35 +0530

    Create stringno.c

---
+++ stringno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+char b[50];
+printf("enter the value of n and b");
+scanf("%s%d",b,n);
+printf("\n %s%d",b,n);
+for(i=1;i<=n;i++);
+{
+printf("%s",b);
+}
+return 0;
+}
