624fc8475717208b6b1534a83b8f7e67b4fc9464
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-07 15:40:47 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int b[10],n,i;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the number");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	if(n=>0&&n<=9)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
