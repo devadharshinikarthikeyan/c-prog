e451726bdb8c2fc3d1474814040ef8177965f922
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-19 16:15:19 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c=a-b;
+    printf("sum is %d",c);
+    if((c%2)==0)
+    {
+        
+    printf("result is even");
+    }
+    else
+    {
+    printf("result is odd");
+}}
