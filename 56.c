6a294eeda1ff27b05aee3aefe284dd1a8b19ab14
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-19 15:56:11 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+int n;
+char a[10];
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the string ");
+scanf("%s",&a[10]);
+g=a[10].n;
+if(((g>='a')&&(g<='z'))&&((g>='A')&&(g<='Z'))&&((g>='0'))&&((g<='9')))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
