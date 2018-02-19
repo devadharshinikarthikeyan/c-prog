a10999e372eeb8f6cf16d3c24844d6cbd4442315
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-19 16:20:19 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    
+   
+    if((a/7)!=0)
+    {
+        
+    printf("result is multiple of 7");
+    }
+    else
+    {
+    printf("result is not multiple of 7");
+}}
