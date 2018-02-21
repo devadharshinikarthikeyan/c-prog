aa4e8e766593f9d01c17f4a6101a827aa1a975bb
devadharshinikarthikeyan <31499230+devadharshinikarthikeyan@users.noreply.github.com>
2018-02-21 16:12:47 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[50];
+    int i,c=0;
+    printf("enter the string :");
+    scanf("%[^n]s",a);
+    for(i=0;s2[i]!='\0';i++){
+if(a[i]==' ')
+        {
+            c++;
+        }
+
+}
+ printf("no of words are :%d",c);
+getch();
+}
